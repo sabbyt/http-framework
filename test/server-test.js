14acb6c27332dc1a3fd60ae32b251a547fa43dd8
suhk <kennethgenesuh@gmail.com>
2016-01-18 15:54:16 -0800

    mocha tests - initial

--- test/server-test.js
+++ test/server-test.js
@@ -1,15 +1,82 @@
-var httpframe = require(__dirname + '/../index');
+var server = require(__dirname + '/temp-server');
+var chai = require('chai');
+var chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
+var expect = chai.expect;
+var request = chai.request;
+var origin = 'localhost:3000';
 
-httpframe.post('/', (req, res) => {
-  httpframe.data(req, (data) => {
-    res.gift(data);
+describe('REST functionality', function() {
+  it('should get', (done) => {
+    request(origin)
+      .get('/rest')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.body.msg).to.eql('get test');
+        done();
+      });
+  });
+
+  it('should post', (done) => {
+    request(origin)
+      .post('/rest')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.body.msg).to.eql('post test');
+        done();
+      });
+  });
+
+  it('should put', (done) => {
+    request(origin)
+      .put('/rest')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.body.msg).to.eql('put test');
+        done();
+      });
+  });
+
+  it('should patch', (done) => {
+    request(origin)
+      .patch('/rest')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.body.msg).to.eql('patch test');
+        done();
+      });
+  });
+
+  it('should delete', (done) => {
+    request(origin)
+      .delete('/rest')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.body.msg).to.eql('delete test');
+        done();
+      });
   });
 });
 
-httpframe.get('/filePath', (req, res) => {
-  httpframe.view(__dirname + '/../public/index.html', res);
-});
+describe('our http-framework test', () => {
+  it('should get and read external files', (done) => {
+    request(origin)
+      .get('/filePath')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.text).to.eql('hello world\r\n');
+        done();
+      });
+  });
 
-httpframe.listen(3000, () => {
-  console.log('server up');
+  it('should post request data', (done) => {
+    request(origin)
+      .post('/')
+      .send('yolo')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.text).to.eql('yolo');
+        done();
+      });
+  });
 });
